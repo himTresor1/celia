47afdfe210baee7374683e553ab2f4c4beaf503d
MURANGWA Pacifique <pacifiquemurangwa001@gmail.com>
2025-12-22 10:00:50 +0200

    refactor: use bcryptjs instead of bcrypt

--- celia-server/src/auth/auth.service.ts
+++ celia-server/src/auth/auth.service.ts
@@ -6,7 +6,7 @@ import {
   NotFoundException,
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 import { PrismaService } from '../prisma/prisma.service';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
